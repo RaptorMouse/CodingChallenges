78f45c7635b9c8c4fb56d5e8383540c383f551de
Robb Hill <robbiebrutishill@gmail.com>
2024-03-14 17:18:37 -0600

    latest update

--- CcJsonParser/JSONParser.py
+++ CcJsonParser/JSONParser.py
@@ -2,47 +2,102 @@ import argparse
 
 class JsonParser():
 
+    lineNum = -1
+    line = ""
+    index = 0
+    lines = []
+    char = ""
+
     def parse(file_name):
         file = open(file_name, "r")
-        if not JsonParser.validJson(file):
-            print("Invalid JSON")
-            return 1
-
-    def validJson(file):
-        lines = file.readlines()
-        if len(lines) == 0:
-            print("Invalid JSON")
-            return False
-        if JsonParser.start(lines[0]):
-            if JsonParser.end(lines[0]):
-                return True
-            else:
-                if JsonParser.end(lines[len(lines)-1]):
-                    return True
-                return False
+        JsonParser.lines = file.readlines()
+        if len(JsonParser.lines) == 0:
+            print("Error: Empty File")
+            exit(1)
+        while JsonParser.lineNum < len(JsonParser.lines):
+            JsonParser.getNextLine()
+            JsonParser.char = JsonParser.line[JsonParser.index]
+            while JsonParser.index < len(JsonParser.line):
+                JsonParser.startSymbol()
+    
+    def getNextLine():
+        if JsonParser.lineNum+1 < len(JsonParser.lines):
+            JsonParser.lineNum += 1
+            JsonParser.index = 0
+            JsonParser.line = JsonParser.lines[JsonParser.lineNum]
         else:
-            return False
+            exit(0)
+    
+    def getNextSymbol():
+        if JsonParser.index+1 < len(JsonParser.line):
+            JsonParser.index += 1
+            JsonParser.char = JsonParser.line[JsonParser.index]
+            return JsonParser.char
+        JsonParser.getNextLine()
+        JsonParser.getNextSymbol()
+
+    def peekNextSymbol():
+        if JsonParser.index+1 < len(JsonParser.line):
+            char = JsonParser.line[JsonParser.index+1]
+            return char
+        JsonParser.getNextLine()
+        JsonParser.peekNextSymbol()
+
+    def startSymbol():
+        match JsonParser.char:
+            case "{":
+                JsonParser.getNextSymbol()
+                if JsonParser.char == "}":
+                    JsonParser.startSymbol(JsonParser.char)
+                    return
+                elif JsonParser.char == "\n":
+                    JsonParser.getNextLine()
+                    JsonParser.getNextSymbol()
+                    return
+                elif JsonParser.char != "\"" or JsonParser.char != "}" or JsonParser.char != "\n":
+                    JsonParser.exitWithError()
+            case "\"":
+                key = ""
+                while JsonParser.peekNextSymbol() != "\"":
+                    key = key + JsonParser.getNextSymbol()
+                JsonParser.getNextSymbol()
+                if JsonParser.getNextSymbol() != ":":
+                    if JsonParser.peekNextSymbol() != " ":
+                        JsonParser.exitWithError()
+                    else:
+                        if JsonParser.getNextSymbol() != ":":
+                            JsonParser.exitWithError()
+                if JsonParser.peekNextSymbol() != "\"":
+                    if JsonParser.getNextSymbol() != " ":
+                        JsonParser.exitWithError()
+                    else:
+                        if JsonParser.getNextSymbol() != "\"":
+                            JsonParser.exitWithError()
+                value = ""
+                while JsonParser.peekNextSymbol() != "\"":
+                    value = value + JsonParser.getNextSymbol()
+                JsonParser.getNextSymbol()
+                if JsonParser.getNextSymbol() != ",":
+                    JsonParser.exitWithError()
+                return
+            case " ":
+                JsonParser.getNextSymbol()
+            case "\n":
+                JsonParser.getNextSymbol()
+        JsonParser.exitWithError()
+        #JsonParser.getNextSymbol()
             
-    def start(line):
-        if "{" in line:
-            print("Begins With {")
-            return True
-
-    def end(line):
-        if "}" in line:
-            print("Ends With }")
-            return True
-
-
+    def exitWithError():
+        print(f"Error: Invalid JSON at Line:%d, Col:%d" %(JsonParser.lineNum+1, JsonParser.index))
+        return exit(1)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Parse JSON files")
-    # parser.add_argument("-c", action="store_true", help="Get byte count of file")
     parser.add_argument("file_name", type=str, nargs="?")
     args = parser.parse_args()
     if args.file_name is None or args.file_name == "":
-        print("No file specified")
+        print("Error: No file specified")
         exit(1)
     file_name = args.file_name
     JsonParser.parse(file_name)
--- CcJsonParser/tests/JSONParserTests.py
+++ CcJsonParser/tests/JSONParserTests.py
@@ -1,13 +1,13 @@
 import unittest
-import JSONParser
+from JSONParser import JsonParser as JP
 
 class JSONTestMethods(unittest.TestCase):
 
     def test_JSON_success(self):
-        self.assertEqual(JSONParser.main("step1\\valid.json"), 0)
+        self.assertEqual(JP.main("step1\\valid.json"), 0)
 
     def test_JSON_failure(self):
-        self.assertNotEqual(JSONParser.main("step1\\invalid.json"), 0)
+        self.assertEqual(JP.main("step1\\invalid.json"), 1)
 
 if __name__ == '__main__':
     unittest.main()
